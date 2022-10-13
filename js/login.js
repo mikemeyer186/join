2548cea2adaac2275a2662f48d135357456e6ada
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-10-13 18:48:20 +0200

    login logo slide added

---
+++ js/login.js
@@ -0,0 +1,11 @@
+function loginInit() {
+    setTimeout(changeColorsOfLoginScreen, 1000);
+}
+
+function changeColorsOfLoginScreen() {
+    document.getElementById('login-page').classList.add('pageColorChange');
+    document.getElementById('login-logo-white').classList.add('logoMovement');
+    document.getElementById('login-logo-white').classList.add('no-opacity');
+    document.getElementById('login-logo-blue').classList.add('logoMovement');
+    document.getElementById('login-logo-blue').classList.remove('no-opacity');
+}
