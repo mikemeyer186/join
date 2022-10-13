f0895608063d7ada1c36d5b128d6a1c10d6f14d2
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-10-13 20:00:40 +0200

    login box styling

--- js/login.js
+++ js/login.js
@@ -8,4 +8,5 @@ function changeColorsOfLoginScreen() {
     document.getElementById('login-logo-white').classList.add('no-opacity');
     document.getElementById('login-logo-blue').classList.add('logoMovement');
     document.getElementById('login-logo-blue').classList.remove('no-opacity');
+    document.getElementById('login-box').classList.remove('no-opacity');
 }
