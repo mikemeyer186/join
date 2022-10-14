39ddd4ebb5575b91466f85e31682530f05af6450
Mike Meier <m0.0m58@yahoo.de>
2022-10-14 12:19:14 +0200

    work interrupted

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,35 +1,45 @@
+// Vorbereitung für Aufteilung unter den Seiten 
+
 function highlightedLegalNotice() {
+    document.getElementById("navSummary").classList.remove('navHighlighted');
+    document.getElementById("navBoard").classList.remove('navHighlighted');
+    document.getElementById("navTask").classList.remove('navHighlighted');
+    document.getElementById("navContacts").classList.remove('navHighlighted');
     document.getElementById("legalNotice").classList.add("legalNoticeHighlighted");
     document.getElementById("legalNoticeInfoIcon").src ="./assets/img/icons/infoIconHighlighted.png";
   }
 function highlightedNavbar(i) {
     let index = i;
-    let summary = document.getElementById("navSummary");
-    let board = document.getElementById("navBoard");
-    let task = document.getElementById("navTask");
-    let contacts = document.getElementById("navContacts"); 
     if (index == 1) {
-    summary.classList.add('navHighlighted');
-    board.classList.remove('navHighlighted');
-    task.classList.remove('navHighlighted');
-    contacts.classList.remove('navHighlighted');
+    document.getElementById("navSummary").classList.add('navHighlighted');
+    document.getElementById("navBoard").classList.remove('navHighlighted');
+    document.getElementById("navTask").classList.remove('navHighlighted');
+    document.getElementById("navContacts").classList.remove('navHighlighted');
+    document.getElementById("legalNotice").classList.remove('legalNoticeHighlighted');
+    document.getElementById("legalNoticeInfoIcon").src ="./assets/img/icons/iconInfo.png";
     }
     if (index == 2) {
-    summary.classList.remove('navHighlighted');
-    board.classList.add('navHighlighted');
-    task.classList.remove('navHighlighted');
-    contacts.classList.remove('navHighlighted');
+    document.getElementById("navSummary").classList.remove('navHighlighted');
+    document.getElementById("navBoard").classList.add('navHighlighted');
+    document.getElementById("navTask").classList.remove('navHighlighted');
+    document.getElementById("navContacts").classList.remove('navHighlighted');
+    document.getElementById("legalNotice").classList.remove('legalNoticeHighlighted');
+    document.getElementById("legalNoticeInfoIcon").src ="./assets/img/icons/iconInfo.png";
     }
     if (index == 3) {
-    summary.classList.remove('navHighlighted');
-    board.classList.remove('navHighlighted');
-    task.classList.add('navHighlighted');
-    contacts.classList.remove('navHighlighted');
+    document.getElementById("navSummary").classList.remove('navHighlighted');
+    document.getElementById("navBoard").classList.remove('navHighlighted');
+    document.getElementById("navTask").classList.add('navHighlighted');
+    document.getElementById("navContacts").classList.remove('navHighlighted');
+    document.getElementById("legalNotice").classList.remove('legalNoticeHighlighted');
+    document.getElementById("legalNoticeInfoIcon").src ="./assets/img/icons/iconInfo.png";
     }
     if (index == 4) {
-    summary.classList.remove('navHighlighted');
-    board.classList.remove('navHighlighted');
-    task.classList.remove('navHighlighted');
-    contacts.classList.add('navHighlighted');
+    document.getElementById("navSummary").classList.remove('navHighlighted');
+    document.getElementById("navBoard").classList.remove('navHighlighted');
+    document.getElementById("navTask").classList.remove('navHighlighted');
+    document.getElementById("navContacts").classList.add('navHighlighted');
+    document.getElementById("legalNotice").classList.remove('legalNoticeHighlighted');
+    document.getElementById("legalNoticeInfoIcon").src ="./assets/img/icons/iconInfo.png";
     }
 }
