bc373754438ebeb61296944d773242e121e24185
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-10-14 20:07:29 +0200

    sign up page added

--- js/login.js
+++ js/login.js
@@ -9,4 +9,14 @@ function changeColorsOfLoginScreen() {
     document.getElementById('login-logo-blue').classList.add('logoMovement');
     document.getElementById('login-logo-blue').classList.remove('no-opacity');
     document.getElementById('login-box').classList.remove('no-opacity');
+    document.getElementById('login-signup-box').classList.remove('no-opacity');
+}
+
+function showSignUpBox() {
+    document.getElementById('login-box').classList.add('d-none');
+    document.getElementById('signup-box').classList.remove('d-none');
+    document.getElementById('login-logo-white').classList.remove('no-opacity');
+    document.getElementById('login-logo-blue').classList.add('no-opacity');
+    document.getElementById('login-page').classList.remove('pageColorChange');
+    document.getElementById('login-signup-box').classList.add('d-none');
 }
