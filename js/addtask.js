f4d9cf7259bfc679f52ff2eb04ddc74edd526c0f
Mike Meier <m0.0m58@yahoo.de>
2022-10-18 20:27:23 +0200

    addTask select update

--- js/addtask.js
+++ js/addtask.js
@@ -1,10 +1,9 @@
-function addTask() {
-    let inputTitle = document.getElementById('inputTitle').value; 
-}
 function changeInputContact() {
     document.getElementById('selectorContact').innerHTML = `<input id="newContactText" type="text" placeholder="Contact email">`; 
 }
-
 function changeInputCategory() {
     document.getElementById('selectorCategory').innerHTML = `<input id="newCategoryText" type="text" placeholder="New category name">`; 
+}
+function addTask() {
+    
 }
