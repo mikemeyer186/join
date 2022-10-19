f01d6467519e9ba0f482cbad2dd0e4a75f1e0e95
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-10-19 20:30:30 +0200

    new contact popup style

--- js/contacts.js
+++ js/contacts.js
@@ -8,3 +8,36 @@ function renderContacts() {
         content.innerHTML += `<div>${contact}</div>`;
     }
 }
+
+function showNewContactPopUp() {
+    document.getElementById('popup-bg').classList.remove('d-none');
+
+    setTimeout(() => {
+        document.getElementById('popup-contact').classList.add('popup-slideIn');
+        document.getElementById('popup-bg').classList.remove('no-opacity');
+    }, 10);
+}
+
+function hideNewContactPopUp() {
+    document.getElementById('popup-contact').classList.remove('popup-slideIn');
+    document.getElementById('popup-bg').classList.add('no-opacity');
+
+    setTimeout(() => {
+        document.getElementById('popup-bg').classList.add('d-none');
+    }, 250);
+}
+
+function blueCloseIcon() {
+    document.getElementById('button-close').src = './assets/img/icons/icon_close_blue.png';
+}
+
+function darkCloseIcon() {
+    document.getElementById('button-close').src = './assets/img/icons/icon_close_dark.png';
+}
+
+/**
+ * stopping propagation of child elements
+ */
+function stopPropagate(event) {
+    event.stopPropagation();
+}
