8b1e754d9b5b75853d401c7c34f99731edd478f4
Mike Meier <m0.0m58@yahoo.de>
2022-10-20 16:16:49 +0200

    code cleaned

--- js/addtask.js
+++ js/addtask.js
@@ -1,25 +1,26 @@
 function changeInputContact() {
-    document.getElementById('selectorContact').innerHTML = `<input id="newContactText" type="text" placeholder="Contact email">`; 
+  document.getElementById("selectorContact").innerHTML = `
+  <input id="newContactText" type="text" placeholder="Contact email">`;
 }
 function changeInputCategory() {
-    document.getElementById('selectorCategory').innerHTML = `<input id="newCategoryText" type="text" placeholder="New category name">`; 
+  document.getElementById("selectorCategory").innerHTML = `
+  <input id="newCategoryText" type="text" placeholder="New category name">`;
 }
 function addTask() {
-    let taskInputTitle = document.getElementById('inputTitle').velue; 
-    let selectContact = document.getElementById('selectContact').value; 
-    let dueDate = document.getElementById('selectDate').value; 
-    let selectCategory = document.getElementById('selectContact').value; 
-    //let taskImportance = document.getElemenentById('').value; 
-    let description = document.getElementById('inputDescription').value; 
+  let taskInputTitle = document.getElementById("inputTitle").velue;
+  let selectContact = document.getElementById("selectContact").value;
+  let dueDate = document.getElementById("selectDate").value;
+  let selectCategory = document.getElementById("selectContact").value;
+  //let taskImportance = document.getElemenentById('').value;
+  let description = document.getElementById("inputDescription").value;
 }
 
-
-function renderSubTask()  {
-    for(let i = 0; i < subTasks.length; i++) {
-        document.getElementById('addSubtaskCheckbox').innerHTML += `
+function renderSubTask() {
+  for (let i = 0; i < subTasks.length; i++) {
+    document.getElementById("addSubtaskCheckbox").innerHTML += `
         <div class="subtaskList">  
-        <input type="checkbox">
+        <input value="${subTasks[i]}" type="checkbox">
         <p>${subTasks[i]}</p>
         </div>`;
-    }
+  }
 }
