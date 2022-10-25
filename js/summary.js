0f110fcf930cecb0b4c8039d44fcfb6bcaa5df12
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-10-25 20:44:58 +0200

    summary page style

---
+++ js/summary.js
@@ -0,0 +1,33 @@
+
+async function summaryInit() {
+    await includeHTML();
+    await loadAccountsFromBackend();
+    loadActiveUserLocal();
+    highlightedNavbar(1);
+    showGreeting();
+}
+
+function showGreeting(){
+    let dateNow = new Date();
+    let hours = dateNow.getHours();
+    let greetingSlogan = returnGreetingSlogan(hours);
+    document.getElementById('greeting-slogan').innerHTML = greetingSlogan;
+    document.getElementById('greeting-name').innerHTML = userAccounts[activeUser].userName;
+}
+
+function returnGreetingSlogan(hours){
+    let greetingSlogan;
+    if (hours < 6 || hours > 22) {
+        greetingSlogan = 'Good night, ';
+    }
+    if (hours >= 6 && hours < 10){
+        greetingSlogan = 'Good morning, ';
+    }
+    if (hours >= 10 && hours < 18){
+        greetingSlogan = 'Have a nice day, ';
+    }
+    if (hours >= 18 && hours <= 22){
+        greetingSlogan = 'Good evening, ';
+    }
+    return greetingSlogan;
+}
