cac691645bb79732793ed562fcba7b6d8c63c08b
MikeMeier97 <m0.0m58@yahoo.de>
2022-10-29 16:07:50 +0200

    board rendering not rdy

--- js/board.js
+++ js/board.js
@@ -1,9 +1,24 @@
 // body onload functions 
 async function boardOnload() {
     await init(2);
-    loadTasksfromBackend();
-    downloadFromServer();
+    await loadTasksfromBackend();
+    await downloadFromServer();
+    renderTasksinBoard();
 }
 function renderTasksinBoard() {
-
+    for(let i; i < tasks.length; i++) {
+            document.getElementById("boardTodo").innerHTML += `
+            <div class="boardBoxContent">
+            <p class="boardBoxCategory">${tasks.taskCategory[i]}</p>
+            <h4 class="boardBoxTitle">${tasks.taskTitle[i]}</h4>
+            <p class="boardBoxDescription">
+              ${tasks.description[i]}
+            </p>
+            <div class="boardBoxFooter">
+              <div class="boxContacts"></div>
+              <img src="./assets/img/icon${tasks.priority[i]}.png" />
+            </div>
+          </div>`;
+        
+    }
 }
