9ee9c0854a926e6e41e015f72a20b07f598f6a84
MikeMeier97 <m0.0m58@yahoo.de>
2022-10-29 20:27:38 +0200

    board render funtion

--- js/board.js
+++ js/board.js
@@ -6,17 +6,17 @@ async function boardOnload() {
     renderTasksinBoard();
 }
 function renderTasksinBoard() {
-    for(let i; i < tasks.length; i++) {
+    for(let i = 0; i < tasks.length; i++) {
             document.getElementById("boardTodo").innerHTML += `
             <div class="boardBoxContent">
-            <p class="boardBoxCategory">${tasks.taskCategory[i]}</p>
-            <h4 class="boardBoxTitle">${tasks.taskTitle[i]}</h4>
+            <p class="boardBoxCategory">${tasks[i].taskCategory}</p>
+            <h4 class="boardBoxTitle">${tasks[i].taskTitle}</h4>
             <p class="boardBoxDescription">
-              ${tasks.description[i]}
+              ${tasks[i].description}
             </p>
             <div class="boardBoxFooter">
               <div class="boxContacts"></div>
-              <img src="./assets/img/icon${tasks.priority[i]}.png" />
+              <img src="./assets/img/icon${tasks[i].priority}.png" />
             </div>
           </div>`;
         
