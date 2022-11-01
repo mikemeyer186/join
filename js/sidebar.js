6f8773bad8f442e9be0a7b43a5e001bc4b9c4a80
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-11-01 13:23:41 +0100

    mobile navigation added

--- js/sidebar.js
+++ js/sidebar.js
@@ -5,15 +5,19 @@
 function highlightedNavbar(item) {
     if (item == 1) {
         highlightSelectedNav('navSummary');
+        highlightSelectedNav('navSummary-mobile');
     }
     if (item == 2) {
         highlightSelectedNav('navBoard');
+        highlightSelectedNav('navBoard-mobile');
     }
     if (item == 3) {
         highlightSelectedNav('navTask');
+        highlightSelectedNav('navTask-mobile');
     }
     if (item == 4) {
         highlightSelectedNav('navContacts');
+        highlightSelectedNav('navContacts-mobile');
     }
     if (item == 5) {
         highlightSelectedNav('legalNotice');
@@ -36,9 +40,10 @@ function highlightSelectedNav(element) {
  * removing class from all elements
  */
 function noHighlightNav() {
-    document.getElementById('navSummary').classList.remove('navHighlighted');
-    document.getElementById('navBoard').classList.remove('navHighlighted');
-    document.getElementById('navTask').classList.remove('navHighlighted');
-    document.getElementById('navContacts').classList.remove('navHighlighted');
-    document.getElementById('legalNotice').classList.remove('navHighlighted');
+    let navElements = document.querySelectorAll('a.sidebarBox');
+
+    for (let i = 0; i < navElements.length; i++) {
+        const element = navElements[i];
+        element.classList.remove('navHighlighted');
+    }
 }
