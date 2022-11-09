f4a3c1c068ae115200d1a2336f14a95da9848bec
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-11-09 22:12:56 +0100

    code cleaning - contacts page ready

--- js/contacts.js
+++ js/contacts.js
@@ -214,7 +214,7 @@ function openContactDetailView(index) {
 }
 
 /**
- * closing setail view when clicking on close icon
+ * closing detail view when clicking on close icon
  */
 function closeContactDetailView(){
     document.getElementById('contacts-right').classList.remove('slided-in');
@@ -326,7 +326,7 @@ function saveAndRenderEdit() {
 
 /**
  * sliding the popup from the bottom of page into view
- * sliding out of view after 3 seconds
+ * sliding out of view after 4 seconds
  * @param {string} id - id of html element
  */
 function slidePopupIntoView(id) {
@@ -348,12 +348,11 @@ function slidePopupIntoView(id) {
         abbreviation: activeUserContacts[index].contactInitials,
         paint: activeUserContacts[index].contactColor
     }];
+
     document.getElementById("selectorContactRenderPopup").innerHTML = ``;
     document.getElementById('addTaskPopup').classList.toggle('translate0');
     document.getElementById('mobiletaskheader').classList.toggle('headerSlideIn');
 }
-
-
 
 /**
  * rendering contacts in addTask popup and selecting contact
@@ -402,30 +401,64 @@ function findContact(name) {
 }
 
 /**
- * save selected contactsPopup
+ * pushing or splicing selected contact in selection of task
+ * @param {string} contactname - name of contact
+ * @param {string} initiales - initials of contact
+ * @param {string} color - rgb color of contact
+ * @param {number} number - id of contact in html-id
  */
-function selectedContactPopup(name, initiales, color, number) {
-    if (document.getElementById("popup" + number + name).classList == "checked") {
-      let index = -1;
-      contactCheckedValue.find(function (name, i) {
-        if (contactCheckedValue.name === name) {
-          index = i;
-        }
-      });
-      contactCheckedValue.splice(index, 1);
-      document.getElementById("popup" + number + name).classList.remove("checked");
-      document.getElementById("popup" + number + name).src = "./assets/img/icons/checkButton.png";
+function selectedContactPopup(contactname, initiales, color, number) {
+    let index = findContactIndex(contactname);
 
+    if (document.getElementById("popup" + number + contactname).classList.contains("checked")) {
+        spliceContact(index, contactname, number);
     } else {
-      contactCheckedValue.push({
-        contactName: name,
+        pushContact(contactname, initiales, color, number);
+    }
+}
+
+/**
+ * splicing selected contact from contactCheckedValue
+ * @param {number} index - index of contact in contactCheckedValue
+ * @param {string} contactname - name of contact
+ * @param {number} number - id of contact in html-id
+ */
+function spliceContact(index, contactname, number) {
+    contactCheckedValue.splice(index, 1);
+    document.getElementById("popup" + number + contactname).classList.remove("checked");
+    document.getElementById("popup" + number + contactname).src = "./assets/img/icons/checkButton.png";
+}
+
+/**
+ * pushing selected contact in contactCheckedValue
+ * @param {string} contactname - name of contact
+ * @param {string} initiales - initials of contact
+ * @param {string} color - rgb color of contact
+ * @param {number} number - id of contact in html-id
+ */
+function pushContact(contactname, initiales, color, number) {
+    contactCheckedValue.push({
+        contactName: contactname,
         abbreviation: initiales,
         paint: color
-      });
-      document.getElementById("popup" + number + name).src = "./assets/img/icons/checkButtonChecked.png";
-      document.getElementById("popup" + number + name).classList.add("checked");
+    });
+    document.getElementById("popup" + number + contactname).src = "./assets/img/icons/checkButtonChecked.png";
+    document.getElementById("popup" + number + contactname).classList.add("checked");
+}
+
+/**
+ * finding index of contact in contactCheckedValue
+ * @param {string} contactname - name of contact
+ * @returns - index
+ */
+function findContactIndex(contactname) {
+    let index;
+    for (let i = 0; i < contactCheckedValue.length; i++) {
+        if (contactCheckedValue[i].contactName == contactname) {
+            index = i;
+        }
     }
-    console.log(contactCheckedValue);
+    return index;
 }
 
 
