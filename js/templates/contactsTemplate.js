a1fb3f08827383d8b0bbf9b4534922ebe89191ec
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2022-11-09 22:24:02 +0100

    sourcing out html-templates of contact page

---
+++ js/templates/contactsTemplate.js
@@ -0,0 +1,112 @@
+/**
+ * html-template for contact card
+ */
+ function contactCardTemplate(contact, index) {
+    return /*html*/ `
+        <div id="contact-id-${index}" class="contact-card" onclick="openContactDetailView(${index})">
+            <div class="contact-pic" style="background-color:${contact.contactColor}">
+                <span class="contact-initials">${contact.contactInitials}</span>
+            </div>
+            <div class="contact-data">
+                <div class="data-name">${contact.contactName}</div>
+                <div class="data-mail">${contact.contactEmail}</div>
+            </div>
+        </div>
+    `;
+}
+
+/**
+ * html-template for alphabetical headline
+ */
+function alphabetCardTemplate(letter) {
+    return /*html*/ `
+    <div class="alphabet-card">
+        <span class="alphabet-letter">${letter}</span>
+    </div>
+`;
+}
+
+/**
+ * html-template for contact detail view
+ */
+function contactDetailViewTemplate(contact, index) {
+    return /*html*/ `
+        <div class="contacts-detail-top">
+            <img class="contact-detail-back" src="./assets/img/arrow_left.png" alt="Back" onclick="closeContactDetailView()">
+            <div class="contact-pic-detail" style="background-color:${contact.contactColor}">
+                <span class="contact-initials-detail">${contact.contactInitials}</span>
+            </div>
+            <div class="contact-detail-name-box">
+                <span class="contact-detail-name">${contact.contactName}</span>
+                <div onclick="showAddTaskPopup()" class="contact-detail-task"><span class="plus">+ </span>Add task</div>
+            </div>
+        </div>
+        <div class="contacts-detail-bottom">
+            <div class="contact-detail-info">
+                <span class="info-headline">Contact information</span>
+                <span class="info-subheadline">Email</span>
+                <a href="mailto:${contact.contactEmail}"><span class="info-email">${contact.contactEmail}</span></a>
+                <span class="info-subheadline">Phone</span>
+                <a href="tel:${contact.contactPhone}"><span class="info-phone">${contact.contactPhone}</span></a>
+            </div>
+            <div class="contact-detail-change" onclick="showEditContactPopUp(${index})">
+                <img class="change-icon" src="./assets/img/icons/icon_edit_contact.png" alt="Edit contact">
+                <img class="change-icon-mobile" src="./assets/img/editButton.png" alt="Edit contact">
+                <span class="change-icon-text">Edit contact</span>
+            </div>
+        </div>
+    `;
+}
+
+/**
+ * html-template for profil pic in edit popup
+ */
+function editProfilPicTemplate(contact) {
+    return /*html*/ `
+        <div class="contact-pic-detail" style="background-color:${contact.contactColor}">
+            <span class="contact-initials-detail">${contact.contactInitials}</span>
+        </div>
+    `;
+}
+
+/**
+ * html-template for checked contacts
+ */
+function checkedContactsTemplate(activeUserContacts, i) {
+    return /*html*/ `
+    <div onclick="selectedContactPopup('${activeUserContacts[i].contactName}','${activeUserContacts[i].contactInitials}','${activeUserContacts[i].contactColor}','${i}')" class="selectorCellContact">
+        <nobr>${activeUserContacts[i].contactName}</nobr>
+        <div id="contactSelectorCheckboxes">
+            <img id="popup${i}${activeUserContacts[i].contactName}" class="checked" src="./assets/img/icons/checkButtonChecked.png">
+        </div>
+    </div>
+`;
+}
+
+/**
+ * html-template for unchecked contacts
+ */
+function uncheckedContactsTemplate(activeUserContacts, i) {
+    return /*html*/`
+        <div onclick="selectedContactPopup('${activeUserContacts[i].contactName}','${activeUserContacts[i].contactInitials}','${activeUserContacts[i].contactColor}','${i}')" class="selectorCellContact">
+        <nobr>${activeUserContacts[i].contactName}</nobr>
+        <div id="contactSelectorCheckboxes">
+            <img id="popup${i}${activeUserContacts[i].contactName}" src="./assets/img/icons/checkButton.png">
+        </div>
+        </div>
+    `;
+}
+
+/**
+ * html-template for new contact
+ */
+function newContactTemplate() {
+    return /*html*/`
+          <div onclick="changeInputContact()" class="selectorCellContact">
+            <nobr>Invite new contact</nobr>
+            <div id="contactSelectorCheckboxes">
+                <img id="contactIconContacts" src="./assets/img/icons/contactIcon.png">
+            </div>
+        </div>
+    `;
+}
