79e6b3baeb16c817ebccccf0c869516965827603
Mike Meyer <112903209+mikemeyer186@users.noreply.github.com>
2023-02-16 22:14:01 +0100

    addtask page redesigned

--- js/addtask.js
+++ js/addtask.js
@@ -1,27 +1,13 @@
 /**
  * body onload functions
  */
-function addTaskOnload() {
-    init(3);
-    renderSubTask();
-    loadTasksfromBackend();
+async function addTaskOnload() {
+    await init(3);
+    await loadTasksfromBackend();
+    showAssignedContacts();
+    hideCloseButton();
 }
 
-/* /**
- * deleting the contact from the contactCheckedValue
- * @param {string} contactName name of contact
- * @param {string} initiales of name
- * @param {color} color of user
- * @param {number} ID of user
- */
-/*
-function selectedContact(contactName, initiales, color, number) {
-    let index = findContactIndex(contactName);
-
-    if (document.getElementById('popup' + number + contactName).classList.contains('checked')) {
-        deletContact(index, contactName, number);
-    } else {
-        addContact(contactName, initiales, color, number);
-    }
+function hideCloseButton() {
+    document.getElementById('addTaskPopopIconClose').classList.add('d-none');
 }
-*/
